d0b7f030e4e59fb60b1bd204a4f3231f22fa87f5
Harun Özceyhan <harunozceyhan@gmail.com>
2020-02-09 03:41:39 +0300

    feat(server): customer model added

---
+++ customer-server/src/db/models/customer.model.ts
@@ -0,0 +1,28 @@
+import { Model } from 'objection'
+
+class Customer extends Model {
+    id!: string
+    firstName!: string
+    lastName!: string
+    birthday!: string
+    gender!: string
+    lastContact!: Date
+    customerLifeTimeValue!: Number
+
+    static get tableName(): string {
+        return 'customer'
+    }
+
+    static get idColumn(): string {
+        return 'id'
+    }
+
+    static get jsonSchema() {
+        return {
+            type: 'object',
+            required: ['firstName', 'lastName', 'birthday', 'gender', 'lastContact', 'customerLifeTimeValue'],
+        }
+    }
+}
+
+export default Customer
--- customer-server/src/migrations/20200209024531_initial_migration.js
+++ customer-server/src/migrations/20200209024531_initial_migration.js
@@ -1,5 +1,5 @@
 exports.up = function(knex) {
-	return knex.raw('CREATE TABLE postgres.customer( id uuid NOT NULL, "firstName" character varying(40) COLLATE pg_catalog."default" NOT NULL, "lastName" character varying(40) COLLATE pg_catalog."default" NOT NULL, birthday date NOT NULL, gender character varying(1) COLLATE pg_catalog."default" NOT NULL, "lastContact" timestamp without time zone NOT NULL, "customerLifetimeValue" real NOT NULL, CONSTRAINT customer_pkey PRIMARY KEY (id) )')
+	return knex.raw('CREATE TABLE customer( id uuid NOT NULL, "firstName" character varying(40) COLLATE pg_catalog."default" NOT NULL, "lastName" character varying(40) COLLATE pg_catalog."default" NOT NULL, birthday date NOT NULL, gender character varying(1) COLLATE pg_catalog."default" NOT NULL, "lastContact" timestamp without time zone NOT NULL, "customerLifetimeValue" real NOT NULL, CONSTRAINT customer_pkey PRIMARY KEY (id) )')
 }
 
 exports.down = function(knex) {}
