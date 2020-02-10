41599881d1e8b8dd683c174e383a556c901af4ae
Harun Özceyhan <harunozceyhan@gmail.com>
2020-02-10 11:56:10 +0300

    fix(server): db initialization port added

--- customer-server/src/db/db.initialize.ts
+++ customer-server/src/db/db.initialize.ts
@@ -10,6 +10,7 @@ class DBInitializer {
             client: 'pg',
             connection: {
                 host: process.env.DB_HOST,
+                port: parseInt(process.env.DB_PORT),
                 user: process.env.DB_USER,
                 password: process.env.DB_PASSWORD,
                 database: process.env.DB_NAME
